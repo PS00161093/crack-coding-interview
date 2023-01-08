b1842e7f66ab5a22fb9b7869c35de45faa0c039d
Prashant Sharma <ps.arraha@gmail.com>
2023-01-08 13:05:41 +0100

    MaxOccurance char | Java8+ functional style

---
+++ java-coding-problems/string-number-math/MaxOccurance.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(maxOccurrenceCharacter("removeCharacter"));
+    }
+
+    private static Map.Entry<Character, Long> MaxOccurance(String str) {
+        if (Objects.isNull(str)) return Map.entry(Character.MIN_VALUE, -1L);
+
+        return str.chars()
+                .filter(ch -> !Character.isWhitespace(ch))
+                .mapToObj(ch -> (char) ch)
+                .collect(Collectors.agroupingBy(ch -> ch, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .orElse(Map.entry(Character.MIN_VALUE, -1L));
+    }
+
+}
