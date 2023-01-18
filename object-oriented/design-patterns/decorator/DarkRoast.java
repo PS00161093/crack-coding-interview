b66ac2beeac8a737d7d48f01d91dd7cf79c0c5cb
Prashant Sharma <ps.arraha@gmail.com>
2023-01-18 22:34:02 +0100

    Decorator pattern exmaple

---
+++ object-oriented/design-patterns/decorator/DarkRoast.java
@@ -0,0 +1,14 @@
+package com.oo.design.patterns.decorator;
+
+public class DarkRoast extends Beverage {
+
+    public DarkRoast() {
+        description = "Dark Roast Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 0.99;
+    }
+
+}
