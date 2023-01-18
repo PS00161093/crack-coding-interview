ab24b935b2176b4c8bbba71ed6f000e6aa3444e4
Prashant Sharma <ps.arraha@gmail.com>
2023-01-18 22:33:21 +0100

    Decorator Pattern example

---
+++ object-oriented/design-patterns/decorator/Beverage.java
@@ -0,0 +1,13 @@
+package com.oo.design.patterns.decorator;
+
+public abstract class Beverage {
+  
+    String description = "Unknown Beverage";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+  
+}
